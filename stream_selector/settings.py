2ec618df6e8368eb854689718181bfe44d6a43db
Himani <global.himani26@gmail.com>
2023-02-13 11:27:33 +0530

    final changes done

--- stream_selector/settings.py
+++ stream_selector/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '&l+$q3n(771w-e%k&4pf&-1dzbz&ep1sz!=6qk)d+1n)5k(drj'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1','10.228.11.20','*']
+ALLOWED_HOSTS = ['localhost','127.0.0.1','10.228.11.20','*','.ngrok.io']
 
 
 # Application definition
